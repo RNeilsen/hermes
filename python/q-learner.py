051dfaffc982849db34b89a2cf073447eaf6b02d
Michael Couch <michael@michaelcouch.io>
2021-07-10 17:48:46 +1000

    feat (python/q-learner) added QLearner, which can select a move to make

--- python/q-learner.py
+++ python/q-learner.py
@@ -35,3 +35,35 @@ class QMatrix(object):
         """
         pass
 
+class QLearner(object):
+
+    """A class implementing a q-learner
+    See https://en.wikipedia.org/wiki/Q-learning
+    """
+
+    def __init__(self, qmatrix=None, randomness=1):
+        """Initialize the class.
+        :qmatrix: A QMatrix instance.
+        :randomness: A scaling factor > 0 indicating how likely less
+                     than optimal moves will be made
+                     (encourages exploration)
+        :returns: None"""
+        self._Q = QMatrix() if qmatrix is None else qmatrix
+        self._randomness = 1
+
+    def _select_move(self, observation):
+        """Select the next move to make somewhat randomly.
+
+        :observation: an observation of the state of the game
+        :returns: The move selected to be made
+
+        """
+        rewards = self._Q[observation]
+        # Softmax weights
+        selection_weights = np.exp(rewards / self._randomness) 
+        move = random.choices(
+            population=rewards.index,
+            weights=selection_weights,
+            k=1)[0]
+        return move
+
