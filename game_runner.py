b48d185664144c2bd7edb84d9db220245c448ac6
Michael Couch <michael@michaelcouch.io>
2021-07-25 16:29:11 +1000

    Added GameRunner class which facilitates gameplay

---
+++ game_runner.py
@@ -0,0 +1,33 @@
+from game import Game
+
+
+class GameRunner(object):
+
+    """The entity which runs a game, e.g. a poker dealer.
+    Responsible for allowing players to interact with the game, facilitating their moves"""
+
+    def __init__(self, game, players):
+        """
+
+        :game: The game the players will play
+        :players: A list of players of the game.
+
+        """
+        self._game = game
+        self._players = {i: player for i, player in enumerate(players)}
+
+    def player_move(self):
+        """TODO: Allow the next player to make their turn.
+        :returns: TODO
+
+        """
+        player_name = self._game.get_next_player()
+        observation = self._game.observe_game(player_name)
+        possible_moves = self._game.get_available_moves()
+        player = self._players[next_player_name]
+        next_move = player.select_move(observation, possible_moves)
+        self._game.make_move(move)
+        score = self._game.get_scoreboard()[player_name]
+        player.update_score(score)
+
+
